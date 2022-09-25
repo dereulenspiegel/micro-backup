de07d43de5d98986750b24ade847ef4466eb4667
Till Klocke <till.klocke@gmail.com>
2022-09-25 17:03:10 +0200

    Making the csi hostpath storage class the default so we don't need to care about setting a storage class in test setups

--- setup-csi-on-kind.sh
+++ setup-csi-on-kind.sh
@@ -12,3 +12,8 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snaps
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
 
 csi-driver-host-path/deploy/kubernetes-latest/deploy.sh
+
+kubectl apply -f csi-driver-host-path/examples/csi-storageclass.yaml
+
+kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
