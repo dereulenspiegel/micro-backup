eaf4ff3a8f24402aa6dfc6c37812735aa761107e
Till Klocke <till.klocke@gmail.com>
2022-09-25 13:45:08 +0200

    now correctly using ginkgo test suite

--- controllers/utils_test.go
+++ controllers/utils_test.go
@@ -5,10 +5,13 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Removing specific index from slice", func() {
-	testSlice := []string{"one", "two", "three"}
-	resultSlice := remove(testSlice, 1)
-	Expect(resultSlice).NotTo(ContainElement("two"))
-	Expect(resultSlice).To(ContainElement("one"))
-	Expect(resultSlice).To(ContainElement("three"))
+var _ = Describe("slice utils", func() {
+	It("should remove specific element", func() {
+		testSlice := []string{"one", "two", "three"}
+		resultSlice := remove(testSlice, 1)
+		Expect(resultSlice).NotTo(ContainElement("two"))
+		Expect(resultSlice).To(ContainElement("one"))
+		Expect(resultSlice).To(ContainElement("three"))
+		Expect(resultSlice).To(HaveLen(2))
+	})
 })
